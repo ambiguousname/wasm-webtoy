a931941684298c6e1400a1217f74a8b8c790e21e
Tyler K <tyly04@gmail.com>
2024-02-25 12:31:47 -0800

    UI improvements

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as cm from "./editor.js";
 
 
 function resizeFromMouse(event) {
-	document.getElementById("text").style.minWidth = event.clientX + "px";
+	document.getElementById("text").style.minWidth = (event.screenX) + "px";
 }
 
 window.onload = async function() {
@@ -11,14 +11,24 @@ window.onload = async function() {
 	let resize = document.getElementById("resize");
 	resize.addEventListener("mousedown", (e) => {
 		e.preventDefault();
+		
+		function remove() {
+			document.removeEventListener("mousemove", resizeFromMouse);
+			game.contentWindow.removeEventListener("mousemove", resizeFromMouse);
+		}
+
 		document.addEventListener("mousemove", resizeFromMouse);
-		document.addEventListener("mouseup", () => document.removeEventListener("mousemove", resizeFromMouse))
+		game.contentWindow.addEventListener("mousemove", resizeFromMouse);
+		document.addEventListener("mouseup", remove);
+		game.contentWindow.addEventListener("mouseup", remove);
 	});
 
+	top.addEventListener("keydown", (e) => {
+		e.stopPropagation();
+	}, true);
+
+	var app = null;
 	function compileWAT(wabt, text) {
-		if (game.src === "about:blank") {
-			return;
-		}
 		try {
 			let result = wabt.parseWat("", text);
 			let bin = result.toBinary({
@@ -34,30 +44,25 @@ window.onload = async function() {
 			let wasm4Cart = game.contentDocument.getElementById("wasm4-cart-json");
 			let json = `{"WASM4_CART":"${encode(bin.buffer)}","WASM4_CART_SIZE":${bin.buffer.length}}`;
 			wasm4Cart.innerText = json;
-			let app = new game.contentWindow.wasm4.App();
+			if (app !== null) {
+				game.contentDocument.body.innerHTML = "";
+			}
+			app = new game.contentWindow.wasm4.App();
 			game.contentDocument.body.appendChild(app);
 		} catch (error) {
 			alert(error);
 			console.error(error);
-			cm.setEditable(cm.editor, true);
 		}
 	}
 
 	let wabt = await new WabtModule();
-	cm.setEditable(cm.editor, false);
 	
 	compileWAT(wabt, cm.editor.state.doc.toString());
 
 	document.getElementById("compile").onclick = function() {
-		cm.setEditable(cm.editor, false);
 		game.src = "player.html";
 		game.onload = function() {
 			compileWAT(wabt, cm.editor.state.doc.toString());
 		}
-	}
-
-	document.getElementById("stop").onclick = function() {
-		game.src = "about:blank"; 
-		cm.setEditable(cm.editor, true);
 	}
 }
